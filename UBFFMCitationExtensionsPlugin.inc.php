272a3348af677d8ca6c203388b08816780e0e381
Thorsten Fritze <1478200+tehaef@users.noreply.github.com>
2021-12-07 16:17:49 +0100

    Fixed typo; spaces instead of tabs for indentation

--- UBFFMCitationExtensionsPlugin.inc.php
+++ UBFFMCitationExtensionsPlugin.inc.php
@@ -3,54 +3,54 @@
 import('lib.pkp.classes.plugins.GenericPlugin');
 
 class UBFFMCitationExtensionsPlugin extends GenericPlugin {
-	/**
-	 * @copydoc Plugin::register()
-	 */
-	public function register($category, $path, $mainContextId = null) {
-		$success = parent::register($category, $path, $mainContextId);
-		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return $success;
-		if ($success && $this->getEnabled($mainContextId)) {
-			HookRegistry::register('CitationStyleLanguage::citationStyleDefaults', array($this, 'addCSLStyle'));
-		}
-		return $success;
-	}
+    /**
+     * @copydoc Plugin::register()
+     */
+    public function register($category, $path, $mainContextId = null) {
+        $success = parent::register($category, $path, $mainContextId);
+        if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return $success;
+        if ($success && $this->getEnabled($mainContextId)) {
+            HookRegistry::register('CitationStyleLanguage::citationStyleDefaults', array($this, 'addCSLStyle'));
+        }
+        return $success;
+    }
 
-	/**
-	 * Add a CSL style to the list of default styles
-	 *
-	 * @param string $hookname
-	 * @param array $args [$defaults, CitationStyleLanguagePlugin]
-	 */
-	public function addCSLStyle($hookName, $args) {
-		$defaults =& $args[0];
+    /**
+     * Add a CSL style to the list of default styles
+     *
+     * @param string $hookname
+     * @param array $args [$defaults, CitationStyleLanguagePlugin]
+     */
+    public function addCSLStyle($hookName, $args) {
+        $defaults =& $args[0];
         $path = Core::getBaseDir() . '/' . $this->getPluginPath() . "/citation-styles";
         $defaults[] = array(
-            'id' => __('pluins.generic.UBFFMCitationExtensions.style.cte-citation-style'),
-			'title' => 'Contributions to Entomolgy Citation Style',
-			'isEnabled' => true,
+            'id' => __('plugins.generic.UBFFMCitationExtensions.style.cte-citation-style'),
+            'title' => 'Contributions to Entomolgy Citation Style',
+            'isEnabled' => true,
             'useCsl' => $path . '/cte-style.csl'
         );
         $defaults[] = array(
-	        'id' => __('pluins.generic.UBFFMCitationExtensions.style.generic-style-rules-linguistics'),
-			'title' => 'Generic Style Rules for Linguistics',
-			'isEnabled' => true,
+            'id' => __('plugins.generic.UBFFMCitationExtensions.style.generic-style-rules-linguistics'),
+            'title' => 'Generic Style Rules for Linguistics',
+            'isEnabled' => true,
             'useCsl' => $path . '/generic-style-rules-for-linguistics.csl',
-		);
-	}
+        );
+    }
 
-	/**
-	 * @copydoc Plugin::getDisplayName()
-	 */
-	function getDisplayName() {
-		return __('plugins.generic.UBFFMCitationExtensions.displayName');
-	}
+    /**
+     * @copydoc Plugin::getDisplayName()
+     */
+    function getDisplayName() {
+        return __('plugins.generic.UBFFMCitationExtensions.displayName');
+    }
 
-	/**
-	 * @copydoc Plugin::getDescription()
-	 */
-	function getDescription() {
-		return __('plugins.generic.UBFFMCitationExtensions.description');
-	}
+    /**
+     * @copydoc Plugin::getDescription()
+     */
+    function getDescription() {
+        return __('plugins.generic.UBFFMCitationExtensions.description');
+    }
 }
 
 ?>
